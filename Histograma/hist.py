4c576472f0b51de4fbc9da951c2bd14255c19237
BrandyLover <30638614+BrandyLover@users.noreply.github.com>
2019-08-02 21:31:27 -0300

    Update hist.py

--- Histograma/hist.py
+++ Histograma/hist.py
@@ -1,8 +1,10 @@
-import cv2 as cv
 import numpy as np 
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
+import cv2 
 
-img = cv.imread("H:\\GitHub\\CVClass\\Images\\Cinza.jpg")
+img = cv2.imread("H:\\GitHub\\CVClass\\Images\\colorida.jpg")
+    
 
 plt.hist(img.ravel(), 256, [0,256])
+cv2.imshow("Imagem", img)
 plt.show()
